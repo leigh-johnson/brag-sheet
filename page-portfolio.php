387b54ff68b29ff85e5c5ddcf82af96f3b025c39
Nuwen <gleighn@gmail.com>
2014-08-06 11:29:15 -0400

    sample copy

--- page-portfolio.php
+++ page-portfolio.php
@@ -26,22 +26,22 @@
 		<figure class="portfolio-item blue ">
 			<img src="http://placehold.it/225x225"></img>
 			<figcaption>
-			<h2>jelly donut</h2>
+			<h2>Jelly Donut</h2>
 			<p class="fig-description">If one examines constructivism, one is faced with a choice: either accept constructivist desituationism or conclude that class, perhaps paradoxically, has intrinsic meaning. </p>
 			<p>Toffee tiramisu</p> <a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item orange ">
 			<img src="http://placehold.it/225x225"></img>
-			<figcaption><h2>jelly donut</h2>
+			<figcaption><h2>Media Outlet</h2>
 			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>
-			<p>Toffee tiramisu </p><a href="#"><button  href="http://placehold.it/500x500">Full</button></a></figcaption>
+			<p>Cake Poptart</p><a href="#"><button  href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item yellow ">
 			<img src="http://placehold.it/225x225"></img>
-			<figcaption><h2>jelly donut</h2>
+			<figcaption><h2>Context</h2>
 			
 			
-			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>
+			<p class="fig-description">The subject is contextualised into a materialist desublimation that includes culture as a paradox.</p>
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item blue ">
@@ -49,7 +49,7 @@
 			<figcaption><h2>jelly donut</h2>
 			
 			
-			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>
+			<p class="fig-description">Foucault uses the term ‘cultural neosemanticist theory’ to denote the role of the observer as artist. </p>
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item orange ">
@@ -57,28 +57,28 @@
 			<figcaption><h2>jelly donut</h2>
 			
 			
-			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>			
+			<p class="fig-description">If one examines cultural narrative, one is faced with a choice: either reject textual neocapitalist theory or conclude that government is capable of intentionality, but only if narrativity is interchangeable with art</p>			
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item yellow ">
 			<img src="http://placehold.it/225x225"></img>
 			<figcaption><h2>jelly donut</h2>
 			
-			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>
+			<p class="fig-description">The primary theme of the works of Rushdie is not narrative as such, but prenarrative.</p>
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item blue ">
 			<img src="http://placehold.it/225x225"></img>
 			<figcaption><h2>jelly donut</h2>
 			
-			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>
+			<p class="fig-description">“Society is part of the absurdity of sexuality,” says Lacan. Lyotard promotes the use of postdialectic desublimation to modify and challenge art. </p>
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure><figure class="portfolio-item orange ">
 			<img src="http://placehold.it/225x225"></img>
 			<figcaption><h2>jelly donut</h2>
 			
 			
-			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>			
+			<p class="fig-description">Any number of narratives concerning cultural neosemanticist theory exist. </p>			
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item yellow ">
@@ -92,7 +92,7 @@
 			<img src="http://placehold.it/225x225"></img>
 			<figcaption><h2>jelly donut</h2>
 			
-			<p class="fig-description">It could be said that if subtextual nationalism holds, we have to choose between constructivist desituationism and submodern narrative. The example of constructivism intrinsic to Pynchon’s V emerges again in Mason & Dixon.</p>
+			<p class="fig-description">The main theme of the works of Rushdie is the failure, and therefore the collapse, of textual class. </p>
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 		<figure class="portfolio-item orange ">
@@ -108,7 +108,7 @@
 			<figcaption><h2>jelly donut</h2>
 			
 			
-			
+			<p class="fig-description">The characteristic theme of Dahmus’s[3] critique of cultural narrative is the dialectic, and thus the genre, of cultural class. </p>
 			<p>Toffee tiramisu </p><a href="#"><button href="http://placehold.it/500x500">Full</button></a></figcaption>
 		</figure>
 
@@ -139,7 +139,7 @@
 			image: {
 				// http://stackoverflow.com/questions/21281263/custom-title-for-magnific-popup
 				titleSrc: function(item) {
-					return item.el.parents('figcaption').find('h2').html() + item.el.parents('figcaption').find('.fig-description').html()
+					return item.el.parents('figcaption').find('h2').html() + "<br />" + item.el.parents('figcaption').find('.fig-description').html()
 				}
 			}
 		});
